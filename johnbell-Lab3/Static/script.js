74f9206c70e6b88f23a50442d54cedad91d71199
JohnEBell2005 <johnbell@andrew.cmu.edu>
2025-03-16 14:58:16 -0400

    Added Lab 3

---
+++ johnbell-Lab3/Static/script.js
@@ -0,0 +1,25 @@
+var x = 5;
+var y = 7;
+var z = x + y;
+console.log(z)
+
+var A = "Hello ";
+var B = "world!";
+var C = A + B;
+console.log(C)
+
+function SumNPrint(x1, x2) {
+    var x3 = x1 + x2;
+    console.log(x3);
+    return x3;
+}
+SumNPrint(x, y);
+SumNPrint(A, B);
+
+if (C.length > z) {
+    console.log(C);
+} else if (C.length < z) {
+    console.log(z);
+} else {
+    console.log("good job!");
+}
